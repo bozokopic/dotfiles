2dd1195f9056695009b4204966a0159eb4819098
bozo.kopic <bozo.kopic@gmail.com>
2020-06-22 18:48:44 +0200

    .

--- terminal.sh
+++ terminal.sh
@@ -2,15 +2,16 @@
 
 CLASSNAME=scratchpad_term
 RESIZE_CMD="resize set 100 ppt 100 ppt"
-MOVE_CMD="move position 0 0"
 
 if ps -C i3 > /dev/null ; then
     CMD=i3-msg
     PROP=instance
-    RESIZE+=", resize shrink height 22 px"
+    RESIZE_CMD+=", resize shrink height 22 px"
+    MOVE_CMD="move position center"
 elif ps -C sway > /dev/null ; then
     CMD=swaymsg
     PROP=app_id
+    MOVE_CMD="move position 0 0"
 else
     exit 1
 fi
