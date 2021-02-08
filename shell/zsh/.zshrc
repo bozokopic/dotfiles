a510034216ce37ac0f82afabe77c38aa058cc3fb
bozo.kopic <bozo@kopic.xyz>
2021-02-08 20:05:52 +0100

    .

--- shell/zsh/.zshrc
+++ shell/zsh/.zshrc
@@ -10,6 +10,8 @@ compinit
 . ~/.dotfiles/shell/zsh/zsh-git-prompt/zshrc.sh
 PROMPT='[%F{green}%n%f@%m %F{green}%~%f $(git_super_status)]$ '
 
+. ~/.dotfiles/shell/zsh/fzf/key-bindings.zsh
+
 . /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 . /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 . /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
--- yay/install.sh
+++ yay/install.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-if ! (which yay > /dev/null); then
-    cd $(dirname "$0")
-    sudo pacman -S --needed --noconfirm git base-devel
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-fi
-
+yay --save --sudo doas
 yay -S --needed --noconfirm \
     $(cat ~/.dotfiles/yay/packages.txt | \
       xargs -I{} sh -c \
         "(pacman -Q {} &> /dev/null) || echo {}")
-yay --save --sudo doas
+
